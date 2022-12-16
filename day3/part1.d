5c952f36127165d542685d5f2f88085be2210dec
lel <88410471+Iamlel@users.noreply.github.com>
2022-12-15 19:47:56 -0800

    Create part1.d

---
+++ day3/part1.d
@@ -0,0 +1,38 @@
+import std.stdio;
+import std.conv;
+import std.algorithm;
+import std.array;
+import std.string;
+
+static int priority(int c) { return c - ((c > 96) ? 96 : 38); }
+
+void main() { 
+    File file = File("input.txt", "r"); 
+
+    int total = 0;
+    int[] input;
+    int median;
+
+    while (!file.eof()) {
+        input = file.readln.chomp.map!(c => priority(to!int(c))).array;
+        
+        median = to!int(input.length * 0.5);
+
+        for (int x = 0; x < median; x++) {
+            if (input[median..$].canFind(input[x])) {
+                median -= input[0..median].count;
+                total += input[x];
+
+                for (int i = 0; i < input.length; i++) {
+                    if (input[i] == 5) {
+                        input.remove(i);
+                        i--;
+                    }
+                }
+            }
+        }
+    }
+
+    writeln(total);
+    file.close(); 
+}
