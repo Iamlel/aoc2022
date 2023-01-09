59b1a28696c6cc6c6a498faa84f186e8f874e4d1
lel <88410471+Iamlel@users.noreply.github.com>
2023-01-08 22:06:53 -0800

    Update part1.rs

--- day04/part1.rs
+++ day04/part1.rs
@@ -4,24 +4,20 @@ const INPUT: &str = "input.txt";
 
 fn main() {
     let mut total = 0;
-    let mut elf1: Vec<u8>;
-    let mut elf2: Vec<u8>;
+    let mut elf1: Vec<i16>;
+    let mut elf2: Vec<i16>;
     let mut sections: Vec<&str>;
 
     for line in fs::read_to_string(INPUT).expect("Should have read the file").lines() {
         sections = line.split(",").collect();
         elf1 = parse_integers(sections[0]);
         elf2 = parse_integers(sections[1]);
-
-        if  (elf1[0] <= elf2[0] && elf1[1] >= elf2[1]) ||
-            (elf2[0] <= elf1[0] && elf2[1] >= elf1[1]) {
-            total += 1;
-        }
+        if (elf1[0] - elf2[0]) * (elf1[1] - elf2[1]) <= 0 { total += 1; }
     }
     println!("The answer is {}", total);
 }
 
-fn parse_integers(section: &str) -> Vec<u8> {
+fn parse_integers(section: &str) -> Vec<i16> {
     return section.split('-')
           .map(|s| s.parse().unwrap())
           .collect();
